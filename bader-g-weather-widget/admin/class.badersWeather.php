bb016198a9f964f7e12f4738ced9b60963e31200
Bader <ghazzawi12@live.com>
2022-09-30 16:45:26 +1000

    Update class.badersWeather.php

--- bader-g-weather-widget/admin/class.badersWeather.php
+++ bader-g-weather-widget/admin/class.badersWeather.php
@@ -37,6 +37,7 @@ class BadersWeather
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
             $result = curl_exec($ch);
             if (curl_errno($ch)) {
                 echo 'Error: ' . curl_error($ch);
